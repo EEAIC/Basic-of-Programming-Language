b93c3888f7d9bc2c59c2b686b30abce8dd2e9905
CMD <mac@JeongKee-MacBook-Pro.local>
2017-10-07 02:04:13 +0900

    Variables and Calculations

---
+++ Variables and Calculations/PrimitiveDataType.java
@@ -0,0 +1,16 @@
+public class PrimitiveDataType {
+    public static void main(String[] args){
+        boolean isFun = false;
+        System.out.println(isFun);
+
+        char c = 'f';
+
+        int x = 59;
+
+        long bing = 345455433L;
+
+        float f = 32.4f;
+
+        double d = 3434343.5;
+    }
+}
